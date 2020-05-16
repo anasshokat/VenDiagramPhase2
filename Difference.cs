54759daf3a42d0529e95dd44704e9929e2e6ace2
umair <umairmehdi13@gmail.com>
2020-05-16 17:36:40 +0500

    updated difference function aded

--- Difference.cs
+++ Difference.cs
@@ -8,5 +8,56 @@ namespace ProjectRedAlert
 {
     class Difference
     {
+        int[] Preform(char Operator, int[] Array1, int[] Array2)
+        {
+            int[] temp = new int[0];
+            sort(ref Array1);
+            sort(ref Array2);
+            int LA1 = Array1.Length;
+            int LA2 = Array2.Length;
+            if (Operator == '⋃')
+            {
+                return (Union(Array1, Array2, LA1, LA2));
+
+            }
+            else if (Operator == '⋂')
+            {
+                return (Intersection(Array1, Array2, LA1, LA2));
+            }
+            else if (Operator == '`' || Operator == '-')
+            {
+                return (Difference(Array1, Array2, LA1, LA2));
+            }
+            else
+            {
+                return temp;
+            }
+        }
+        int[] Difference(int[] Array1, int[] Array2, int LA1, int LA2)
+        {
+            int[] result = new int[LA1 + LA2];
+            int i = 0, j = 0, index = 0;
+            bool flag = true;
+            while (i < LA1)
+            {
+                for (j = 0; j < LA2;)
+                {
+                    if (Array1[i] == Array2[j])
+                    {
+                        flag = false;
+                        break;
+                    }
+                    j++;
+                }
+                if (flag == true)
+                {
+                    result[index] = Array1[i];
+                    index++;
+                }
+                flag = true;
+                i++;
+            }
+            return result;
+        }
     }
 }

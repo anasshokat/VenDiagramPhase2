6031d911c5133dc4ab0038e923c2230ea4f8aa72
Taha <57385903+tahakhan009@users.noreply.github.com>
2020-05-16 16:31:16 +0500

    Add files via upload

--- Unions.cs
+++ Unions.cs
@@ -8,5 +8,72 @@ namespace ProjectRedAlert
 {
     class Unions
     {
+        // Union Method 
+        int[] Union(int[] Array1, int[] Array2, int LA1, int LA2)
+        {
+            int[] result = new int[LA1 + LA2];
+            int index = 0, i = 0, j = 0;
+            while (i < LA1 && j < LA2)
+            {
+                if (Array1[i] < Array2[j])
+                {
+                    if (Array1[i] != 0)
+                    {
+                        result[index++] = Array1[i++];        
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                }
+                else if (Array2[j] < Array1[i])
+                {
+                    if (Array2[j] != 0)
+                    {
+                        result[index++] = Array2[j++];
+                    }
+                    else
+                    {
+                        j++;
+                    }
+                }
+                else
+                {
+                    if (Array2[j] != 0)
+                    {
+                        result[index++] = Array2[j++];
+                        i++;
+                    }
+                    else
+                    {
+                        j++; i++;
+                    }
+                }
+                
+            }
+            while (i < LA1)
+            {
+                if (Array1[i] != 0)
+                {
+                    result[index++] = Array1[i++];     
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            while (j < LA2)
+            {
+                if (Array2[j] != 0)
+                {
+                    result[index++] = Array2[j++];
+                }
+                else
+                {
+                    j++;
+                }
+            }
+            return result;
+        }
     }
 }
